f641cc77aebf51ea7646438c4b278b7e56e40d58
CatHead132 <cathead1323@gmail.com>
2023-06-01 21:43:17 +0300

    Edit CatHub.lua

--- CatHub.lua
+++ CatHub.lua
@@ -58,8 +58,9 @@ ScriptsSection:NewButton("Eaclipse", "Possible to crash", function()
 	local a,b,c,d,e=loadstring,request or http_request or (http and http.request) or (syn and syn.request),assert,tostring,"https://api.eclipsehub.xyz/auth"c(a and b,"Executor not Supported")a(b({Url=e.."\?\107e\121\61"..d(mainKey),Headers={["User-Agent"]="Eclipse"}}).Body)()
 end)
 local Tab = Window:NewTab("Customize")
+local CustomizeSection = Tab:NewSection("Customize")
 for theme, color in pairs(themes) do
-    customizeSection:NewColorPicker(theme, "Change your "..theme, color, function(color3)
+    CustomizeSection:NewColorPicker(theme, "Change your "..theme, color, function(color3)
         Library:ChangeColor(theme, color3)
     end)
 end
