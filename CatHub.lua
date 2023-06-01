7cc95b4d24f81babe3f27f66536fe4485b4c4308
CatHead132 <cathead1323@gmail.com>
2023-06-01 21:52:40 +0300

    a

--- CatHub.lua
+++ CatHub.lua
@@ -57,6 +57,13 @@ ScriptsSection:NewButton("Eaclipse", "Possible to crash", function()
 	getgenv().mainKey = "nil"
 	local a,b,c,d,e=loadstring,request or http_request or (http and http.request) or (syn and syn.request),assert,tostring,"https://api.eclipsehub.xyz/auth"c(a and b,"Executor not Supported")a(b({Url=e.."\?\107e\121\61"..d(mainKey),Headers={["User-Agent"]="Eclipse"}}).Body)()
 end)
+local Tab = Window:NewTab("Customize")
+local CustomizeSection = Tab:NewSection("Customize")
+for theme, color in pairs(themes) do
+    customizeSection:NewColorPicker(theme, "Change your "..theme, color, function(color3)
+        Library:ChangeColor(theme, color3)
+    end)
+end
 
 local Tab = Window:NewTab("Creator")
 local creatorSection = Tab:NewSection("Creator")
