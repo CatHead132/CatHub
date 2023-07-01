a8a170e74cba943a670c1d7148110bd985808aae
CatHead132 <116957800+CatHead132@users.noreply.github.com>
2023-07-01 23:18:47 +0300

    Update asdsdılcszx.lua

--- asdsdılcszx.lua
+++ asdsdılcszx.lua
@@ -1,42 +1,40 @@
 while true do
-  game.Workspace.Terrain.WaterWaveSize = 0
-  game.Workspace.Terrain.WaterWaveSpeed = 0
-  game.Workspace.Terrain.WaterReflectance = 0
-  game.Workspace.Terrain.WaterTransparency = 0
+game.Workspace.Terrain.WaterWaveSize = 0
+game.Workspace.Terrain.WaterWaveSpeed = 0
+game.Workspace.Terrain.WaterReflectance = 0
+game.Workspace.Terrain.WaterTransparency = 0
+game.Lighting.GlobalShadows = false
+game.Lighting.FogEnd = 9e9
+game.Lighting.Brightness = 0
+settings().Rendering.QualityLevel = "Level01"
 
-  game.Lighting.GlobalShadows = false
-  game.Lighting.FogEnd = 9e9
-  game.Lighting.Brightness = 0
-
-  settings().Rendering.QualityLevel = Enum.QualityLevel.Level01
-
-  for _, f in pairs(game:GetDescendants()) do
-    if f:IsA("BasePart") then
-      f.Material = Enum.Material.Plastic
-      f.Reflectance = 0
-    elseif f:IsA("Decal") or f:IsA("Texture") then
-      f.Transparency = 0
-    elseif f:IsA("ParticleEmitter") or f:IsA("Trail") then
-      f.Lifetime = NumberRange.new(0)
-    elseif f:IsA("Explosion") then
-      f.BlastPressure = 0
-      f.BlastRadius = 0
-    elseif f:IsA("Fire") or f:IsA("SpotLight") or f:IsA("Smoke") or f:IsA("Sparkles") then
-      f.Enabled = false
-    elseif f:IsA("MeshPart") then
-      f.Material = Enum.Material.Plastic
-      f.Reflectance = 0
-      f.TextureID = "10385902758728957"
+for _, part in pairs(game:GetDescendants()) do
+    if part:IsA("Part") or part:IsA("UnionOperation") or part:IsA("CornerWedgePart") or part:IsA("TrussPart") then
+        part.Material = "Plastic"
+        part.Reflectance = 0
+    elseif part:IsA("Decal") or part:IsA("Texture") then
+        part.Transparency = 0
+    elseif part:IsA("ParticleEmitter") or part:IsA("Trail") then
+        part.Lifetime = NumberRange.new(0)
+    elseif part:IsA("Explosion") then
+        part.BlastPressure = 0
+        part.BlastRadius = 0
+    elseif part:IsA("Fire") or part:IsA("SpotLight") or part:IsA("Smoke") or part:IsA("Sparkles") then
+        part.Enabled = false
+    elseif part:IsA("MeshPart") then
+        part.Material = "Plastic"
+        part.Reflectance = 0
+        part.TextureID = 10385902758728957
     end
-  end
+end
 
-  for _, g in pairs(game.Lighting:GetDescendants()) do
-    if g:IsA("PostEffect") then
-      g.Enabled = false
+for _, effect in pairs(game.Lighting:GetChildren()) do
+    if effect:IsA("BlurEffect") or effect:IsA("SunRaysEffect") or effect:IsA("ColorCorrectionEffect")
+        or effect:IsA("BloomEffect") or effect:IsA("DepthOfFieldEffect") then
+        effect.Enabled = false
     end
-  end
+end
 
-
-
-  wait(1) -- Add a short delay to avoid excessive CPU usage
+sethiddenproperty(game.Lighting, "Technology", "Compatibility")
+  wait(1)
 end
